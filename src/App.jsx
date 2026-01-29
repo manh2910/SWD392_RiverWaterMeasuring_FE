3e088d76c049a2e4fa667d7eb9bdd7a7feaa86db
VietHQ767 <viethqse182482@fpt.edu.vn>
2026-01-29 16:09:02 +0700

    Admin

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import AdminLayout from "./layouts/AdminLayout";
+
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Rivers from "./pages/Rivers/Rivers";
+import Stations from "./pages/Stations/Stations";
+import Hubs from "./pages/Hubs/Hubs";
+import Sensors from "./pages/Sensors/Sensors";
+import Parameters from "./pages/Parameters/Parameters";
+import Observations from "./pages/Observations/Observations";
+import DataPackages from "./pages/DataPackages/DataPackages";
+
+function App() {
+  return (
+    <Routes>
+      {/* ADMIN */}
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="rivers" element={<Rivers />} />
+        <Route path="stations" element={<Stations />} />
+        <Route path="hubs" element={<Hubs />} />
+        <Route path="sensors" element={<Sensors />} />
+        <Route path="parameters" element={<Parameters />} />
+        <Route path="observations" element={<Observations />} />
+        <Route path="data-packages" element={<DataPackages />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
